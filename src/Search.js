b9565495a7e6588fbc1d1d0ec69f007101723443
VMevamax <132147870+VMevamax@users.noreply.github.com>
2023-11-27 00:49:03 +0200

    css center

--- src/Search.js
+++ src/Search.js
@@ -45,9 +45,7 @@ export default function WeatherSearch() {
           <li>Description: {weather.description}</li>
           <li>Humidity: {weather.humidity}%</li>
           <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
+          <img src={weather.icon} alt={weather.description} />
         </ul>
       </div>
     );
